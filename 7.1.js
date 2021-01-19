c7f8e172060a39a12737318b6743b29f6a66f807
Oliver <oliverjamesphillips@gmail.com>
2021-01-19 14:07:28 +0000

    Comment out initial tests

--- 7.1.js
+++ 7.1.js
@@ -13,7 +13,7 @@ function countVowels(string) {
 // Uncomment these tests to check your solution
 // =====
 
-test(countVowels("abc"), 1);
-test(countVowels("abracadabra"), 5);
-test(countVowels("rhythm"), 0);
-test(countVowels("a1e%i}o/u"), 5);
+// test(countVowels("abc"), 1);
+// test(countVowels("abracadabra"), 5);
+// test(countVowels("rhythm"), 0);
+// test(countVowels("a1e%i}o/u"), 5);
